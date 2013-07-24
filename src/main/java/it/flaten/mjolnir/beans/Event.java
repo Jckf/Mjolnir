60616054f992eb4d94c72a158781eca0c08b1dff
Jim Flaten <jim@flaten.it>
2013-07-24 11:42:09 +0200

    Improved NativeStorage's efficiency.

--- src/main/java/it/flaten/mjolnir/beans/Event.java
+++ src/main/java/it/flaten/mjolnir/beans/Event.java
@@ -7,76 +7,76 @@ import javax.persistence.Table;
 import java.io.Serializable;
 
 @Entity
-@Table(name = "mjolnir_events")
+@Table(name = "Event")
 public class Event implements Serializable {
-	public enum EventType {
-		UNBAN(0), BAN(1);
+    public enum EventType {
+        UNBAN(0), BAN(1);
 
-		private int id;
+        private int id;
 
-		private EventType(int id) {
-			this.id = id;
-		}
+        private EventType(int id) {
+            this.id = id;
+        }
 
-		public Integer getId() {
-			return this.id;
-		}
-	}
+        public Integer getId() {
+            return this.id;
+        }
+    }
 
-	@Id		private int			id;
-	@Column	private int			time;
-	@Column private String		player;
-	@Column private String		op;
-	@Column private EventType	type;
-	@Column private String		reason;
-	@Column private int			expires;
+    @Id        private int            id;
+    @Column    private int            time;
+    @Column private String        player;
+    @Column private String        op;
+    @Column private EventType    type;
+    @Column private String        reason;
+    @Column private int            expires;
 
-	public void setId(int id) {
-		this.id = id;
-	}
-	public int getId() {
-		return this.id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return this.id;
+    }
 
-	public void setTime(int time) {
-		this.time = time;
-	}
-	public int getTime() {
-		return this.time;
-	}
+    public void setTime(int time) {
+        this.time = time;
+    }
+    public int getTime() {
+        return this.time;
+    }
 
-	public void setPlayer(String player) {
-		this.player = player;
-	}
-	public String getPlayer() {
-		return this.player;
-	}
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+    public String getPlayer() {
+        return this.player;
+    }
 
-	public void setOp(String op) {
-		this.op = op;
-	}
-	public String getOp() {
-		return this.op;
-	}
+    public void setOp(String op) {
+        this.op = op;
+    }
+    public String getOp() {
+        return this.op;
+    }
 
-	public void setType(EventType type) {
-		this.type = type;
-	}
-	public EventType getType() {
-		return this.type;
-	}
+    public void setType(EventType type) {
+        this.type = type;
+    }
+    public EventType getType() {
+        return this.type;
+    }
 
-	public void setReason(String reason) {
-		this.reason = reason;
-	}
-	public String getReason() {
-		return this.reason;
-	}
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+    public String getReason() {
+        return this.reason;
+    }
 
-	public void setExpires(int expires) {
-		this.expires = expires;
-	}
-	public int getExpires() {
-		return this.expires;
-	}
+    public void setExpires(int expires) {
+        this.expires = expires;
+    }
+    public int getExpires() {
+        return this.expires;
+    }
 }
--- src/main/java/it/flaten/mjolnir/storages/NativeStorage.java
+++ src/main/java/it/flaten/mjolnir/storages/NativeStorage.java
@@ -1,6 +1,6 @@
 package it.flaten.mjolnir.storages;
 
-import com.avaje.ebean.Query;
+import com.avaje.ebean.*;
 import it.flaten.mjolnir.Mjolnir;
 import it.flaten.mjolnir.beans.Event;
 
@@ -13,103 +13,102 @@ import java.util.List;
  * @author Jim Flaten
  */
 public class NativeStorage implements Storage {
-	/**
-	 * Plugin instance.
-	 *
-	 * A place to store a reference to the running plugin.
-	 */
-	private Mjolnir plugin;
+    /**
+     * Plugin instance.
+     *
+     * A place to store a reference to the running plugin.
+     */
+    private Mjolnir plugin;
 
-	/**
-	 * Constructor.
-	 *
-	 * Instantiates {@link NativeStorage}.
-	 *
-	 * @param plugin A reference to the running plugin.
-	 */
-	public NativeStorage(Mjolnir plugin) {
-		this.plugin = plugin;
-	}
+    /**
+     * Constructor.
+     *
+     * Instantiates {@link NativeStorage}.
+     *
+     * @param plugin A reference to the running plugin.
+     */
+    public NativeStorage(Mjolnir plugin) {
+        this.plugin = plugin;
+    }
 
-	/**
-	 * Create database tables.
-	 *
-	 * Tries to cound the number of {@link Event}s in the database, and
-	 * invokes {@link it.flaten.mjolnir.Mjolnir#installDDL()} if an @{link PersistenceException} is thrown.
-	 */
-	@Override
-	public void createTables() {
-		try {
-			this.plugin.getDatabase().find(Event.class).findRowCount();
-		} catch (PersistenceException exception) {
-			this.plugin.installDDL();
-		}
-	}
+    /**
+     * Create database tables.
+     *
+     * Tries to count the number of {@link Event}s in the database, and
+     * invokes {@link it.flaten.mjolnir.Mjolnir#installDDL()} if an @{link PersistenceException} is thrown.
+     */
+    @Override
+    public void createTables() {
+        try {
+            this.plugin.getDatabase().find(Event.class).findRowCount();
+        } catch (PersistenceException exception) {
+            this.plugin.installDDL();
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Event saveEvent(String player,String op,Event.EventType type,String reason,int expires) {
-		Event event = this.plugin.getDatabase().createEntityBean(Event.class);
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Event saveEvent(String player,String op,Event.EventType type,String reason,int expires) {
+        Event event = this.plugin.getDatabase().createEntityBean(Event.class);
 
-		event.setTime((int) (System.currentTimeMillis() / 1000L));
-		event.setPlayer(player);
-		event.setOp(op);
-		event.setType(type);
-		event.setReason(reason);
-		event.setExpires(expires);
+        event.setTime((int) (System.currentTimeMillis() / 1000L));
+        event.setPlayer(player);
+        event.setOp(op);
+        event.setType(type);
+        event.setReason(reason);
+        event.setExpires(expires);
 
-		this.plugin.getDatabase().save(event);
+        this.plugin.getDatabase().save(event);
 
-		return event;
-	}
+        return event;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public List<Event> loadEvents(String player) {
-		Query<Event> query = this.plugin.getDatabase().find(Event.class);
-		query.where().eq("player",player);
-		query.orderBy("time");
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Event> loadEvents(String player) {
+        Query<Event> query = this.plugin.getDatabase()
+            .find(Event.class)
+            .where()
+                .ieq("player",player)
+            .orderBy("id ASC");
 
-		List<Event> events = query.findList();
+        List<Event> events = query.findList();
 
-		if (events == null || events.size() == 0) {
-			return null;
-		}
+        if (events == null || events.size() == 0) {
+            return null;
+        }
 
-		return events;
-	}
+        return events;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Event loadActiveEvent(String player) {
-		Query<Event> query = this.plugin.getDatabase().find(Event.class);
-		query.where().eq("player",player);
-		query.where().ge("time",(int) (System.currentTimeMillis() / 1000L));
-		query.orderBy("time");
-		query.setMaxRows(1);
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Event loadActiveEvent(String player) {
+        return this.plugin.getDatabase()
+            .find(Event.class)
+            .where()
+                .ieq("player",player)
+                .disjunction()
+                    .eq("expires",0)
+                    .ge("expires",(int) (System.currentTimeMillis() / 1000L))
+            .orderBy("id DESC")
+            .setMaxRows(1)
+            .findUnique();
+    }
 
-		List<Event> events = query.findList();
+    /**
+     * Does nothing.
+     *
+     * The native Bukkit database does not need to be shut down.
+     */
+    @Override
+    public void shutdown() {
 
-		if (events == null || events.size() == 0) {
-			return null;
-		}
-
-		return events.get(0);
-	}
-
-	/**
-	 * Does nothing.
-	 *
-	 * The native Bukkit database does not need to be shut down.
-	 */
-	@Override
-	public void shutdown() {
-
-	}
+    }
 }
